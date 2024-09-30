a40793c6d8be478b21fcddf17ecf28d13ee07d09
Alejandro Rojo <alejandrorojo483@gmail.com>
2024-09-29 21:38:02 -0300

    Segundo listado casi completo

---
+++ segundoListado/F.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+int main(){
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> obstaculosTecho(n, 0);
+    vector<int> obstaculosPiso(n, 0);
+}
